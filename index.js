edb393577eddde9d565c577d8c1afa7f69f741c7
Fayza Salman Muhammad <princessfayza97@gmail.com>
2018-07-25 21:49:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,4 @@ const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random() * 20;
 
 const randomInteger = Math.floor(randomNumber);
+const randomUserID = randomInteger + 1000000001;
